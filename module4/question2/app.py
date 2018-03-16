a62e32c8ef9b34501a968ef13f2a8b15ec1b500e
AsherMeyers <anm39@cornell.edu>
2018-03-16 15:12:21 -0700

    Update app.py

--- module4/question2/app.py
+++ module4/question2/app.py
@@ -8,7 +8,6 @@ import pandas as pd
 app = dash.Dash()
 
 df = pd.read_csv("https://raw.githubusercontent.com/AsherMeyers/DATA-608/master/module4/data.csv")
-    #'https://gist.githubusercontent.com/chriddyp/cb5392c35661370d95f300086accea51/raw/8e0768211f6b747c0db42a9ce9a0937dafcbd8b2/indicators.csv')
 
 # convert date strings to datetime objects
 df['Date'] = pd.to_datetime(df['Date'])
